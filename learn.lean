2abab0b465465f50b705fc2470055f1c16462224
darsh <darshverma@gmail.com>
2025-12-30 00:14:22 +0530

    1.2

--- learn.lean
+++ learn.lean
@@ -9,3 +9,13 @@
 #eval String.append (String.append "A" "B") "C" -- ABC
 #eval if 3 == 3 then 5 else 7 -- 5
 #eval if 3 == 4 then "equal" else "≠" -- ≠
+
+-- 1.2 - types
+-- Nat is the type for natural numbers
+#eval (1 - 2 : Nat) -- evals to 0
+-- Int is for integers
+#eval (1 - 2 : Int) -- evals to -1
+#check (1 - 2 : Nat) -- returns 1 - 2 : Nat. even without specifying Nat, we get Nat. so it's probably good to use Int.
+
+
+
